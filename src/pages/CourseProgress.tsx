c48a754fccbbc08808a7fe1b7204effbc2699c4b
Juan Yepes <yepes.j@hotmail.com>
2021-11-06 20:08:36 -0400

    updated the course progress page styling

--- src/pages/CourseProgress.tsx
+++ src/pages/CourseProgress.tsx
@@ -1,12 +1,12 @@
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
 // import CardMedia from "@mui/material/CardMedia";
-import Typography from '@mui/material/Typography';
-import { Button } from '@mui/material';
-import { useContext, useEffect, useState } from 'react';
-import { useNavigate } from 'react-router';
-import { UserContext } from '../context/UserContext';
-import { getQuestions, Question } from '../services/question';
+import Typography from "@mui/material/Typography";
+import { Button } from "@mui/material";
+import { useContext, useEffect, useState } from "react";
+import { useNavigate } from "react-router";
+import { UserContext } from "../context/UserContext";
+import { getQuestions, Question } from "../services/question";
 
 export const CourseProgress = () => {
   const navigate = useNavigate();
@@ -16,7 +16,7 @@ export const CourseProgress = () => {
   }>({});
   useEffect(() => {
     if (!user) {
-      navigate('/login');
+      navigate("/login");
     }
   }, [user]);
 
@@ -44,30 +44,29 @@ export const CourseProgress = () => {
   return (
     <div
       style={{
-        display: 'flex',
-        backgroundColor: '#272262',
-        height: '100vh',
-        flexDirection: 'column',
+        display: "flex",
+        backgroundColor: "#272262",
+        flexDirection: "column",
       }}
     >
-      <h1 style={{ display: 'flex', justifyContent: 'center', color: 'white' }}>
+      <h1 style={{ display: "flex", justifyContent: "center", color: "white" }}>
         Course Progress
       </h1>
       <div
         style={{
-          display: 'flex',
-          flexDirection: 'row',
-          justifyContent: 'space-evenly',
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "space-around",
         }}
       >
         {Object.keys(questions).map((i) => {
           return (
-            <Card>
+            <Card style={{ margin: "25px" }}>
               <CardContent
                 style={{
-                  margin: '10px',
-                  padding: '10px',
-                  justifyContent: 'space-around',
+                  margin: "25px",
+                  padding: "10px",
+                  justifyContent: "space-around",
                 }}
               >
                 <Typography gutterBottom variant="h2" component="div">
@@ -75,18 +74,18 @@ export const CourseProgress = () => {
                 </Typography>
                 <div
                   style={{
-                    display: 'flex',
-                    justifyContent: 'space-between',
+                    display: "flex",
+                    justifyContent: "space-between",
                   }}
                 >
                   {Object.keys(questions[i as any]).map((j) => {
                     return (
-                      <div style={{ margin: '50px' }}>
+                      <div style={{ margin: "50px" }}>
                         <Typography
                           gutterBottom
                           variant="h3"
                           component="div"
-                          style={{ marginTop: '30px' }}
+                          style={{ marginTop: "30px" }}
                         >
                           Day {j}
                         </Typography>
@@ -95,24 +94,24 @@ export const CourseProgress = () => {
                             <Card
                               key={question._id}
                               style={{
-                                display: 'flex',
-                                flexDirection: 'row',
-                                justifyContent: 'space-evenly',
-                                maxWidth: '275px',
-                                height: '200px',
+                                display: "flex",
+                                flexDirection: "row",
+                                justifyContent: "space-evenly",
+                                maxWidth: "275px",
+                                height: "200px",
                               }}
                             >
                               <CardContent
                                 style={{
-                                  display: 'flex',
-                                  flexDirection: 'column',
-                                  justifyContent: 'space-between',
+                                  display: "flex",
+                                  flexDirection: "column",
+                                  justifyContent: "space-between",
                                 }}
                               >
                                 <Typography
                                   style={{
-                                    fontSize: '18px',
-                                    marginBottom: '25px',
+                                    fontSize: "18px",
+                                    marginBottom: "25px",
                                   }}
                                 >
                                   Question: {question.title}
@@ -120,7 +119,7 @@ export const CourseProgress = () => {
                                 <Button
                                   color="secondary"
                                   variant="contained"
-                                  style={{ color: 'white' }}
+                                  style={{ color: "white" }}
                                   onClick={() =>
                                     navigate(`/code-challenge/${question._id}`)
                                   }
